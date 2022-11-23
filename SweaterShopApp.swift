6b4744323d5e7eb1a1e990359294a262fe77892d
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-11-23 07:47:40 -0600

    Create SweaterShopApp.swift

---
+++ SweaterShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShopApp.swift
+//  SweaterShop
+//
+//  Created by Jayce Sagvold on 11/22/22.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
